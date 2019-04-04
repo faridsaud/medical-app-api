1fc637ee391b41e1bb53add4a80f305bc0c347d2
Farid Saud <faridsaud93@gmail.com>
2019-04-04 18:24:45 -0500

    Refactor: Separate app code

---
+++ src/app.js
@@ -0,0 +1,62 @@
+import 'dotenv/config';
+import jwt from 'fastify-jwt';
+import validator from 'validator';
+import db from './config/db';
+import swagger from './config/swagger';
+import {nonAuthRoutes} from './routes';
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true,
+});
+
+// Swagger
+fastify.register(require('fastify-swagger'), swagger.options);
+
+// JWT
+fastify.register(jwt, {
+  secret: process.env.SECRET,
+});
+
+// Cookies
+fastify.register(require('fastify-cookie'));
+
+// Auth
+fastify
+.decorate('verifyJWT', function (req, reply, done) {
+  try {
+    const accessToken = req.cookies.accessToken;
+    const {email, uuid} = fastify.jwt.decode(accessToken);
+    if(!validator.isEmail(email)){
+      done(new Error());
+    }
+    if(!validator.isUUID(uuid)){
+      done(new Error());
+    }
+    done();
+  }catch (e) {
+    done(e);
+  }
+
+})
+.register(require('fastify-auth'));
+
+// Routes that doesn't requires to be authenticated
+nonAuthRoutes.forEach((route) => {
+  fastify.route(route)
+});
+
+// Routes that requires to be authenticated
+fastify.register(
+    require('./plugins/auth')
+);
+
+
+
+
+
+db.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
+
+export default fastify;
