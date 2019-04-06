5d749e384d67b03247f1f417d0c543e164db1d7a
Farid Saud <faridsaud93@gmail.com>
2019-04-05 21:25:43 -0500

    Feat: Add fallback for port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fastify from './app';
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT);
+    await fastify.listen(process.env.PORT || 5000);
     fastify.swagger();
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
